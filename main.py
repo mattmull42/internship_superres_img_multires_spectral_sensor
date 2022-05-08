57f3fb9298e6c40240ef70230f130c60000f792a
mattmull42 <mattmull06@protonmail.com>
2022-05-08 16:53:51 +0000

    Added the errors log file.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 from os import listdir
+import csv
 
 from src.pipeline_class import *
 from src.utilities.plots import *
@@ -18,10 +19,26 @@ def main(argv):
     else:
         input_names = argv[1:]
 
-    for input_name in input_names:
-        pipeline_bayer.run(input_name)
-        pipeline_quad.run(input_name)
-        pipeline_binning.run(input_name)
+    create_output_dirs()
+    
+    with open(path.join('output', 'errors_log.csv'), 'w') as errors_log:
+        csvwriter = csv.writer(errors_log)
+        csvwriter.writerow(['Name', 'MSE red error', 'MSE green error', 'MSE blue error', 'SSIM red error', 'SSIM green error', 'SSIM blue error'])
+
+        for input_name in input_names:
+            pipeline_bayer.run(input_name)
+            pipeline_quad.run(input_name)
+            pipeline_binning.run(input_name)
+
+            mse_bayer = pipeline_bayer.mse_errors
+            mse_quad = pipeline_quad.mse_errors
+            mse_binning = pipeline_binning.mse_errors
+            ssim_bayer = pipeline_bayer.ssim_errors
+            ssim_quad = pipeline_quad.ssim_errors
+            ssim_binning = pipeline_binning.ssim_errors
+            csvwriter.writerow([input_name[:-4] + '_bayer'] + mse_bayer + ssim_bayer)
+            csvwriter.writerow([input_name[:-4] + '_quad_bayer'] + mse_quad + ssim_quad)
+            csvwriter.writerow([input_name[:-4] + '_quad_bayer_binning'] + mse_binning + ssim_binning)
 
 
 if __name__ == "__main__":
